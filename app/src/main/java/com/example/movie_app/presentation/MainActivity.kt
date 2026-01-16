022bf356b034599f0ff9389986113b96fa0ab925
Rostyslav bc2025-4 <suhoverskijrostislav@gmail.com>
2026-01-17 01:15:46 +0200

    1. Integrated core libraries: Hilt, Retrofit, Coil and Coroutines 2. Set up API key access through BuildConfig 3. Established project directory structure (Data, Domain and Presentation layers)

---
+++ app/src/main/java/com/example/movie_app/presentation/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.movie_app.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.movie_app.presentation.theme.Movie_appTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Movie_appTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Movie_appTheme {
+        Greeting("Android")
+    }
+}
